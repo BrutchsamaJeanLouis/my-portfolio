863318766e135b7dcf87684ce482f08cc7eab948
BrutchsamaJeanLouis <73585384+BrutchsamaJeanLouis@users.noreply.github.com>
2022-08-11 09:16:54 +0100

    test env update

--- App.jsx
+++ App.jsx
@@ -3,7 +3,7 @@ const Navbar = window.Navbar.default
 export default function App() {
   const [state, setState] = React.useState('red')
   return (
-    <div style={{ backgroundColor: state }} onClick={() => setState('green')}>Hey
+    <div style={{ backgroundColor: state }} onClick={() => state === 'green' ? setState('red') : setState('green')}>Hey
       <Navbar />
     </div>
   )
