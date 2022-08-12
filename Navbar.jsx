c3621687e4f00cc479dc33221d9e9be5f12983ab
BrutchsamaJeanLouis <brutchsama14@gmail.com>
2022-08-12 19:28:32 +0100

    test redux and react hooks working

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,7 +1,15 @@
 // import React from 'react'
 
 export default function Navbar() {
+  const dispatch = ReactRedux.useDispatch()
+  const githubLink = ReactRedux.useSelector((state) => state.githubProfileLink)
   return (
-    <div>Navbar</div>
+    <div>
+      Navbar
+      <button onClick={() => dispatch({ type: 'TEST_CHANGE_GIT_LINK', payload: 'hi' })}>
+        change link
+        {githubLink}
+      </button>
+    </div>
   )
 }
